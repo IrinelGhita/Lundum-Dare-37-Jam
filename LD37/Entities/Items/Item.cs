e0023e8d65dc2c978f2689b5f17c212ee628cbce
Da-Dude <al_doilea_id@yahoo.com>
2016-12-10 11:43:56 +0000

    Added items and Effects abstract classes

---
+++ LD37/Entities/Items/Item.cs
@@ -0,0 +1,39 @@
+﻿using LD37.Entities.Non_Tangible;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LD37.Entities
+{
+    abstract class Item :AbstractEntity
+    {
+        #region Variables
+
+        public String name { get; set; }
+        public String description { get; set; }
+        public String value { get; set; }
+
+        #endregion
+
+        #region Constructors
+        public Item() : base()
+        {
+        }
+        #endregion
+
+        #region Private methods
+
+        #endregion
+
+        #region Public methods
+
+        public virtual Effect Use(AbstractEntity entity)
+        {
+
+            return new Effect();
+        }
+
+        #endregion
+    }
+}
---
+++ LD37/Entities/Non-Tangible/Effect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LD37.Entities.Non_Tangible
+{
+    class Effect
+    {
+        public Effect()
+        {
+
+        }
+    }
+}
