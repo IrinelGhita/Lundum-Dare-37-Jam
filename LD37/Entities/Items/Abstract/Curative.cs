50377f79c559a98e057e45de19636c66060e1d57
Da-Dude <al_doilea_id@yahoo.com>
2016-12-10 13:04:09 +0000

    Restructured classes

---
+++ LD37/Entities/Items/Abstract/Curative.cs
@@ -0,0 +1,35 @@
+﻿using LD37.Entities.Non_Tangible;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LD37.Entities.Items
+{
+    class Curative:Item
+    {
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+        public Curative() : base()
+        {
+        }
+        #endregion
+
+        #region Private methods
+
+        #endregion
+
+        #region Public methods
+
+        public override Effect Use(GameEntity entity)
+        {
+
+            return new Effect(entity);
+        }
+
+        #endregion
+    }
+}
